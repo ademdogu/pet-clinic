a8432fa36a1616e86edc608fa2fda6eb15bfcc0d
Dogu <Dogua@labcorp.com>
2020-07-19 13:06:50 -0700

    adding files

---
+++ src/main/java/com/springframework/asdpetclinic/AsdPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.asdpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/asdpetclinic/AsdPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.asdpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
