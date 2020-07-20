6ce1720a960be6154d8320a9b6d6eabf80d27432
Dogu <Dogua@labcorp.com>
2020-07-20 16:03:00 -0700

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.springframework.asdclinic.services;
+
+import com.springframework.asdclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
