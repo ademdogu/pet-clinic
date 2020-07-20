5675884fb8a13c4343b915f30c947c3a2d52bba6
Dogu <Dogua@labcorp.com>
2020-07-19 17:14:51 -0700

    adding POJOs. Closes #2.

---
+++ src/main/java/com/springframework/asdpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.asdpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/asdpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.asdpetclinic.model;
+
+public class Vet extends Person {
+}
