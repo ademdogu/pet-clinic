87162ce1e74c4b0929fdd0bfe0835f5e343c4f28
Adem Dogu <dogua@labcorp.com>
2020-07-21 16:22:52 -0700

    added contact info to owner

--- pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
@@ -4,7 +4,43 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String phone;
+    private String email;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public Set<Pet> getPets() {
         return pets;
