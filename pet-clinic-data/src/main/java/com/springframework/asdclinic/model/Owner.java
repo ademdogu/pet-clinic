3e05c0b49158eff9009d9fb678935c85bdf51fe5
Adem Dogu <dogua@labcorp.com>
2020-07-22 13:27:47 -0700

    adding jpa annotations to models

--- pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
@@ -1,14 +1,26 @@
 package com.springframework.asdclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "email")
     private String email;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
