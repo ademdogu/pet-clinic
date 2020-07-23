bf323cdb50bfcead145716ba5c3eaa8192dc3a4a
Adem Dogu <dogua@labcorp.com>
2020-07-23 16:49:04 -0700

    adding lombok to project

--- pet-clinic-data/src/main/java/com/springframework/asdclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/model/BaseEntity.java
@@ -1,25 +1,23 @@
 package com.springframework.asdclinic.model;
 
+import lombok.*;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public boolean isNew(Long id) {
         return id == null;
--- pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/model/Owner.java
@@ -1,12 +1,28 @@
 package com.springframework.asdclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address,
+                 String city, String phone, String email, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.phone = phone;
+        this.email = email;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String address;
@@ -23,43 +39,4 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
