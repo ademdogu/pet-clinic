0be5d2a80da6e031ee7ffd3ad53450cd73e48ce4
Adem Dogu <dogua@labcorp.com>
2020-07-22 21:35:53 -0700

    adding visit map serivce and visit jpa

---
+++ pet-clinic-data/src/main/java/com/springframework/asdclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.springframework.asdclinic.services;
+
+import com.springframework.asdclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
--- pet-clinic-web/src/main/java/com/springframework/asdclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/asdclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.springframework.asdclinic.bootstrap;
 
 import com.springframework.asdclinic.model.*;
-import com.springframework.asdclinic.services.OwnerService;
-import com.springframework.asdclinic.services.PetTypeService;
-import com.springframework.asdclinic.services.SpecialtyService;
-import com.springframework.asdclinic.services.VetService;
+import com.springframework.asdclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -93,6 +93,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners...");
 
