ec6f438828f31553e99d1dd6bc62026987e91a40
Adem Dogu <dogua@labcorp.com>
2020-07-24 10:43:14 -0700

    removed not-needed plugin from pons for junit test

---
+++ pet-clinic-data/src/test/java/com/springframework/asdclinic/model/PetTest.java
@@ -0,0 +1,19 @@
+package com.springframework.asdclinic.model;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTest {
+
+    @BeforeAll
+    public void setup() {
+
+    }
+
+    @Test
+    void getName() {
+    }
+}
--- pet-clinic-web/src/test/java/asdpetclinic/AsdPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/asdpetclinic/AsdPetClinicApplicationTests.java
@@ -2,8 +2,11 @@ package asdpetclinic;
 
 import com.springframework.asdclinic.AsdPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = AsdPetClinicApplication.class)
 class AsdPetClinicApplicationTests {
 
